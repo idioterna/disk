f5615a58e38e9662e234c2f501152132e475fc85
idiot <jure.koren@zemanta.com>
2013-11-05 16:34:23 +0100

    Fix

--- disk.sh
+++ disk.sh
@@ -5,7 +5,7 @@ ata=$1
 disk=$(ls -l /sys/block/ | grep $(grep "^${ata##ata}" /sys/class/scsi_host/host*/unique_id | sed "s/.*\(host[0-9]\{1,\}\).*/\1/") | awk '{print $8}')
 
 if [ -z "$disk" ] ; then
-	echo "nema."
+	echo "$ata not found."
 else
 	echo /dev/$disk
 fi
